7888fb43010f55d440d73254770a68e9a5523d1e
joseph <seidel.jp@gmail.com>
2016-07-20 11:16:56 -0500

    initial commit

---
+++ models.py
@@ -0,0 +1,73 @@
+#home 1 or 0
+#opponent
+#runs
+#opp_runs
+#gameid
+#date
+
+#$  sudo -u postgres createdb penaax
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Sequence, ForeignKey
+from sqlalchemy.orm import sessionmaker
+from sqlutils import get_or_create
+import calendar
+
+engine = create_engine('postgresql://postges:joseph@localhose/penaax')
+
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+
+class Game(Base):
+    __tablename__ = 'game'
+    id = Column(Integer, Sequence("game_id_seq"), primary_key=True)
+    home = Column(Integer)
+    oppo = Column(String)
+    pens_runs = Column(Integer)
+    oppo_runs = Column(Integer)
+    gameid = Column(String)
+    lookupdate = Column(String)
+
+    def parse_info(self, info, lookupdate):
+        self.lookupdate = lookupdate
+        for i in info:
+            if "Pensacola" in i['team']:
+                self.pens_runs = int(i['runs'])
+                if "home" in i['location']:
+                    self.home = 1
+                if "away" in i['location']:
+                    self.home = 0
+            else:
+                self.oppo_runs = int(i['runs'])
+                self.oppo = str(i['team'])
+
+class LookupDate(Base):
+    __tablename__ = "date"
+    id = Column(Integer, Sequence("game_id_seq"), primary_key=True)
+    yyyymmdd = Column(String)
+    finished = Column(Integer)
+
+Base.metadata.create_all(engine)
+
+s = Session()
+
+_MONTHS = [4, 5, 6, 7, 8, 9]
+_YEARS = [2012, 2013, 2014, 2015, 2016]
+
+def build_dates():
+    dates = []
+    for year in _YEARS:
+        for month in _MONTHS[1:-1]:
+            for day in range(1, calendar.monthrange(year, month)[1]+1):
+                dates.append("{}{}{}".format(year, str(month).zfill(2), str(day).zfill(2)))
+    return dates
+
+
+if __name__ == "__main__":
+    dates = build_dates()
+    for date in dates:
+        d, c = get_or_create(s, LookupDate, yyyymmdd=date)
+    s.commit()
+
+print len(s.query(Game).all())
---
+++ run.py
@@ -0,0 +1,56 @@
+import calendar
+from selenium import webdriver
+from models import Session, Game
+from pyvirtualdisplay import Display
+from sqlutils import get_or_create
+
+display = Display(visible=0, size=(800,600))
+display.start()
+
+
+session = Session()
+_URL = "http://www.milb.com/scoreboard/index.jsp?cid=&lid=111&org=&sc=&sid=milb&t=league&ymd="
+
+# p.errmsg "There are no games scheduled for this league on July 19, 2016. Please select another date."
+
+def find_game_tables(browser):
+    tables = browser.find_elements_by_tag_name("table")
+    tIds = []
+    for table in tables:
+        if "_penaax_" in table.get_attribute("id"): tIds.append(table.get_attribute("id"))
+    return tIds
+
+def parse_row(row):
+    location = row.get_attribute('class')
+    team = row.find_element_by_class_name('team_info').find_element_by_class_name('a').text
+    runs = row.find_element_by_class_name('runs').text
+
+    return dict(location=location, team=team, runs=runs)
+
+def main():
+    browser = webdriver.Firefox()
+#This will iterate over yyyymmdd
+    dates = build_dates()
+    for date in session.query(LookupDate).filter(LookupDate.finished != 1):
+        browser.get(_URL + date.yyyymmdd)
+        gameTableIds = find_game_tables(browser)
+        for gameId in gameTableIds:
+            table = browser.find_element_by_id(gameId)
+            tbody = table.find_element_by_tag_name('tbody')
+            tableRows = tbody.find_elements_by_tag_name('tr')
+            gameInfo = []
+            for tableRow in tableRows:
+                gameInfo.append(parse_row(tableRow))
+            game, created = get_or_create(session, Game, gameId)
+            game.parse_info(gameInfo, date.yyyymmdd)
+        date.finished = 1
+        session.commit()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+display.stop()
